a8fb8d87742c0e7a68223a325ba53f970ac97103
ACERLEANDRO\leo_d <leo1996.dasilva@gmail.com>
2022-10-16 17:21:34 -0300

    EX1

---
+++ javascript.js
@@ -0,0 +1,24 @@
+while (true) {
+  let idade = prompt("Coloque a sua idade. ou escreva PARE para parar");
+
+  if (idade.toUpperCase == "PARE") {
+    alert("O programa parou.");
+    break;
+  } else {
+    idade = Number(idade);
+  }
+
+  if (isNaN(idade) || idade < 0) {
+    alert("Idade invalida tente novamente.");
+    continue;
+  }
+
+  if (idade <= 15) {
+    alert("E um jovem")
+  } else if (idade <= 64) {
+    alert("E um adulto")
+  } else {
+    alert("E um idoso")
+  }
+  
+}
